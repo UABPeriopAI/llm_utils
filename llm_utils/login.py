c4a405acc1c7a78670dd8ef35475c8474b24d79b
Kameshwari <ksoundararajan@uabmc.edu>
2024-04-30 12:58:32 -0500

    adding end_point variable

--- llm_utils/login.py
+++ llm_utils/login.py
@@ -13,28 +13,11 @@ class BYOK_Handler():
         st.success('API key is valid.')
         os.environ["OPENAI_API_KEY"] = api_key
 
-    def _api_key_validation(self, api_key):
+    def _api_key_validation(self, api_key, end_point):
         pass
 
-    def initialize_api_key(self, api_key, api_key_type):
-        self.api_key = api_key
-        self.api_key_type = api_key_type
-
-        if self.api_key_type == "Azure":
-            key_handler = AzureKeyHandler(self.CHAT, self.PUBMED_CHAT)
-        elif self.api_key_type == "OpenAI":
-            key_handler = OpenaiKeyHandler(self.CHAT, self.PUBMED_CHAT)
-        else:
-            st.error("Select the API key type.")
-            return False
-
-        initialized = key_handler._api_key_validation(self.api_key)
-
-        if initialized:
-            self._incorporate_api_key(self.api_key)
-            return True
-        else:
-            return False
+    def initialize_api_key(self, api_key, end_point):
+        pass
 
     def get_chat_function(self):
         return self.CHAT
@@ -46,17 +29,17 @@ class BYOK_Handler():
 
 class AzureKeyHandler(BYOK_Handler):
 
-    def _api_key_validation(self, api_key):
+    def _api_key_validation(self, api_key, end_point):
         # TODO: timeout
-        response = requests.get('https://nlp-ai-svc.openai.azure.com/openai/models?api-version=2024-02-01', headers={ 'api-key': api_key})
+        response = requests.get(end_point + '/openai/models?api-version=2024-02-01', headers={ 'api-key': api_key})
 
         if response.status_code == 200:
             return True
         else:
             return False
 
-    def initialize_api_key(self, api_key):
-        if self._api_key_validation(api_key):
+    def initialize_api_key(self, api_key, end_point):
+        if self._api_key_validation(api_key, end_point):
             self._incorporate_api_key(api_key)
             return True
 
@@ -68,8 +51,8 @@ class AzureKeyHandler(BYOK_Handler):
 class OpenaiKeyHandler(BYOK_Handler):
 
 
-    def _api_key_validation(self, api_key):
-        response = requests.get('https://api.openai.com/v1/engines', headers={ 'Authorization': 'Bearer ' + api_key})
+    def _api_key_validation(self, api_key, end_point):
+        response = requests.get(end_point, headers={ 'Authorization': 'Bearer ' + api_key})
 
         if response.status_code == 200:
             return True
@@ -78,8 +61,8 @@ class OpenaiKeyHandler(BYOK_Handler):
             print(f"Response: {response.text}")
             return False
 
-    def initialize_api_key(self, api_key):
-        if self._api_key_validation(api_key):
+    def initialize_api_key(self, api_key, end_point):
+        if self._api_key_validation(api_key, end_point):
             self._incorporate_api_key(api_key)
             return True
         else:
