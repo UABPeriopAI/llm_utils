352d5cb8d1bc53bcc43a2f7ffee91078aa0de468
Ryan Godwin <rcg2@uab.edu>
2024-06-06 16:08:10 -0500

    Removing FastAPI components from Upload manager

--- llm_utils/api_utils/UploadManager.py
+++ llm_utils/api_utils/UploadManager.py
@@ -1,16 +1,11 @@
 import tempfile
-from pathlib import Path
 import pandas as pd
 import pypandoc
 import streamlit as st
-from fastapi import HTTPException
-from io import BytesIO
 import pandas as pd
 import tempfile
 import pypandoc
-from typing import Union, Any
-import base64
-from abc import abstractmethod
+
 
 class UploadManager:
 
@@ -48,59 +43,3 @@ class StreamlitUploadManager(UploadManager):
                 return pypandoc.convert_file(tmpfile.name, "markdown"), extension
         return None, None
 
-
-class FastAPIUploadManager(UploadManager):
-    def process_file_bytes(self, file: bytes, extension: str) -> Union[pd.DataFrame, str]:
-        """
-        Reads the file from byte string based on the file extension and returns
-        either a DataFrame or a markdown string.
-        
-        Args:
-            file_bytes (bytes): The byte-encoded content of the file.
-            extension (str): The file extension indicating the file type.
-
-        Returns:
-            Union[pd.DataFrame, str]: Depending on the file extension, either returns a DataFrame for Excel files
-            or a markdown string for DOCX files.
-        """
-        print("Processing file with extension - ", extension)
-        
-        if extension == ".xlsx":
-            print("Opening Excel file")
-            return pd.read_excel(BytesIO(file))
-        elif extension == ".docx":
-            print("Converting Word file to Markdown")
-            with tempfile.NamedTemporaryFile(delete=True, suffix=".docx") as tmpfile:
-                tmpfile.write(file)
-                tmpfile.seek(0)
-                return pypandoc.convert_file(tmpfile.name, "markdown")
-
-    def read_and_validate_file(self, encoded_file: str, extension: str) -> Any:
-        """
-        The function reads and validates a base64 encoded file, then processes it based on the specified
-        file extension.
-        
-        Args:
-          encoded_file (str): The `encoded_file` parameter is a string that represents the file content
-        encoded in base64 format. This function reads and validates the file content by decoding it from
-        base64 and then passing it to the `read_file` method for further processing based on the specified
-        file extension. If any errors occur during
-          extension (str): Extension refers to the file format or type of the file being processed. It is
-        typically represented by a file extension such as ".txt", ".pdf", ".jpg", etc. In the context of the
-        provided code snippet, the extension parameter is used to specify the file format of the decoded
-        file before further
-        
-        Returns:
-          The `read_and_validate_file` method returns the output of the `read_file` method if it is not
-        None. If the `read_file` method returns None, a HTTPException with status code 422 and detail
-        "Failed to process the file" is raised. If any other exception occurs during the process, a
-        HTTPException with status code 500 and the exception message is raised.
-        """
-        try:
-            file_bytes = base64.b64decode(encoded_file)
-            output = self.process_file_bytes(file_bytes, extension)
-            if output is None:
-                raise HTTPException(status_code=422, detail="Failed to process the file")
-            return output
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e)) from e
